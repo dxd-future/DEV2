4e065ad7be2995d935cc1496c97727eff70d28a3
dxd-future <obuhovsemen797@gmail.com>
2025-03-02 01:22:44 +0900

    5.0

--- functions.js
+++ functions.js
@@ -1,13 +1,20 @@
 let n = 1;
-
 let back_lvl = () => {
     if(n > 1){
         n -= 1;
-    console.log(n);
     document.querySelector(`#tag${n}`).style.display = 'flex';
     document.querySelector(`#tag${n + 1}`).style.display = 'none';
     }
-    document.querySelector('#num').textContent = n
+    if(n != 1){
+        document.querySelector('#num1').textContent = n - 1;
+    }else{
+        document.querySelector('#num1').textContent = '';
+    }
+    if(n != 8){
+        document.querySelector('#num2').textContent = n + 1;
+    }
+    document.querySelector('#num').textContent = n;
+
 }
 let next_lvl = () => {
     if(n < 8){
@@ -15,10 +22,21 @@ let next_lvl = () => {
         document.querySelector(`#tag${n - 1}`).style.display = 'none';
         document.querySelector(`#tag${n}`).style.display = 'flex';
     }
-    document.querySelector('#num').textContent = n
+    if(n != 1){
+        document.querySelector('#num1').textContent = n - 1;
+    }
+    if(n != 8){
+        document.querySelector('#num2').textContent = n + 1;
+    }else{
+        document.querySelector('#num2').textContent = '';
+    }
+    document.querySelector('#num').textContent = n;
 }
-let a = () => {
+let run_code = () => {
     document.querySelector('.table').innerHTML += `${editor.getValue()}`;
 }
-
-setInterval(a , 500);
+let clear_code = () => {
+    console.log(1);
+    
+    document.querySelector('.table').innerHTML = '';
+}
--- main.js
+++ main.js
@@ -6,3 +6,5 @@ editor.session.setMode("ace/mode/html"); // Установка языка
 // setInterval(a, 1000)
 document.querySelector('#back_lvl').addEventListener('click', back_lvl)
 document.querySelector('#next_lvl').addEventListener('click', next_lvl)
+document.querySelector('#clear_code_btn').addEventListener('click', clear_code)
+document.querySelector('#run_code_btn').addEventListener('click', run_code)
