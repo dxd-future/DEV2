b3685f5f393ecd4c41e0c8eaf053f5d2839cb7de
dxd-future <obuhovsemen797@gmail.com>
2025-04-16 19:26:17 +0900

    2.1

--- functions.js
+++ functions.js
@@ -1,42 +1,62 @@
 let n = 1;
+let edit = () => {
+    if(editor.getValue() != ''){
+        document.querySelector('.update_code_btn').disabled = false;
+    }
+}
 let back_lvl = () => {
     if(n > 1){
-        n -= 1;
-        document.querySelector(`#tag${n}`).style.display = 'flex';
-        document.querySelector(`#tag${n + 1}`).style.display = 'none';
+        n--;
     }
+    document.querySelector('.info').textContent = directory.html[n - 1].info;
+    document.querySelector('#num').textContent = n;
     if(n != 1){
         document.querySelector('#num1').textContent = n - 1;
     }else{
         document.querySelector('#num1').textContent = '';
     }
-    if(n != 8){
+    if(n != len){
         document.querySelector('#num2').textContent = n + 1;
     }
-        document.querySelector('#num').textContent = n;     
-
+    document.querySelector('#num').textContent = n;
+    document.querySelector('.tag-container').innerHTML = '';
+    if(directory.html[n - 1].tag != ''){
+        document.querySelector('.tag-container').innerHTML = `
+        <${directory.html[n - 1].tag} class="test-tag">${directory.html[n - 1].tag_content}</${directory.html[n - 1].tag}>
+        `;
+    }
+    // if(directory.html[n - 1].type != ''){
+    //     document.querySelector('.tag-container').innerHTML = `
+    //     <${directory.html[n - 1].tag} class="test-tag" type="${directory.html[]}">${directory.html[n - 1].tag_content}</${directory.html[n - 1].tag}>
+    //     `;
+    // }
 }
 let next_lvl = () => {
-    if(n < 8){
-        n += 1;
-        document.querySelector(`#tag${n - 1}`).style.display = 'none';
-        document.querySelector(`#tag${n}`).style.display = 'flex';
+    if(n < directory.html.length){
+        n++;
     }
+    document.querySelector('#num').textContent = n;
+    document.querySelector('.info').textContent = directory.html[n - 1].info;
     if(n != 1){
         document.querySelector('#num1').textContent = n - 1;
     }
-    if(n != 8){
+    if(n != directory.html.length){
         document.querySelector('#num2').textContent = n + 1;
     }else{
         document.querySelector('#num2').textContent = '';
     }
-        document.querySelector('#num').textContent = n;
+    document.querySelector('.tag-container').innerHTML = '';
+    if(directory.html[n - 1].tag != ''){
+        document.querySelector('.tag-container').innerHTML = `
+        <${directory.html[n - 1].tag} class="test-tag">${directory.html[n - 1].tag_content}</${directory.html[n - 1].tag}>
+        `;
+    }
 }
-let run_code = () => {
+
+let update_code = () => {
     document.querySelector('.table').innerHTML = '';
     document.querySelector('.table').innerHTML += `${editor.getValue()}`;
-    
-}
-let clear_code = () => {
-    document.querySelector('.table').innerHTML = '';
+    if(document.querySelector('.table').innerHTML != ''){
+        document.querySelector('.update_code_btn').textContent = "Обновить"
+    }
 }
--- main.js
+++ main.js
@@ -6,26 +6,71 @@ editor.session.setMode("ace/mode/html"); // Установка языка
 // setInterval(a, 1000)
 document.querySelector('#back_lvl').addEventListener('click', back_lvl)
 document.querySelector('#next_lvl').addEventListener('click', next_lvl)
-document.querySelector('#clear_code_btn').addEventListener('click', clear_code)
-document.querySelector('#run_code_btn').addEventListener('click', run_code)
+document.querySelector('.update_code_btn').addEventListener('click', update_code)
 
-// let directory = {
-//     html:[
-//         {
-//             id:"1",
-//             title: "",
-            
-//         },
-//         {
-            
-//         },
-//         {
+let directory = {
+    html:[
+        {
+            id: 1,
+            info: "В начале работы тебе необходимо создать структуру приложения, делается это с помощью тегов. Тег — это метка у контента. Метки объясняют, что это за контент и о чем он. Но прежде чем начать ознакомся с этими самыми тегами.",
+            tag: "",
+            tag_content: "",
+            type: ""
+        },
+        {
+            id: 2,
+            info: "Класс(class) тега — это атрибут который задаёт имя любому элементу на странице. Подходит любому тегу, в дальнейшем будет необходим.",
+            tag: "",
+            tag_content: "",
+            type: ""
+        },
+        {
+            id: 3,
+            info: "Заголовок(h1) - элемент предназначенный для объявления заголовка.",
+            tag: "h1",
+            tag_content: "Текст",
+            type: ""
+        },
+        {
+            id: 4,
+            info: "Div — это элемент контейнера, используемый для группировки и упорядочивания тегов на веб-странице.",
+            tag: "div",
+            tag_content: "Содержимое",
+            type: ""
+        },
+        {
+            id: 5,
+            info: "Кнопка(Button) - элемент который позволяет пользователям взаимодействовать с веб-страницей, выполняя определенные действия.",
+            tag: "button",
+            tag_content: "Кнопка",
+            type: ""
+        },
+        {
+            id: 6,
+            info: "Тип(type) тега — это атрибут который задаёт тип любому элементу на странице.",
+            tag: "",
+            tag_content: "",
+            type: ""
+        },
+        {
+            id: 7,
+            info: "Инпут(input) - элемент, который предназначен для создания текстовых полей, различных кнопок, переключателей и флажков.",
+            tag: "input",
+            tag_content: "",
+            type: ""
+        },
+        {
+            id: 8,
+            info: "Инпут(input) - элемент, который предназначен для создания текстовых полей, различных кнопок, переключателей и флажков.",
+            tag: "input",
+            tag_content: "",
+            type: ""
+        }
+    ],
+    css:[
 
-//         }           
-//     ],
-//     css:[
+    ],
+    js:[
 
-//     ],
-//     js:[
-//     ]
-// }
+    ]
+}
